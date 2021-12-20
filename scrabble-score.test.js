c7c14bc1b5941abe9731a93734c446a6a5fcce74
Rob Styles <rob@styles.to>
2021-12-20 12:09:15 +0000

    Link to jest docs

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,5 +1,8 @@
 const scrabbleScore = require("./scrabble-score");
 
+// List of matchers (expects) for jest:
+// https://jestjs.io/docs/using-matchers
+
 describe("scrabble score", () => {
   it("should score 'a' correctly", () => {
     expect(scrabbleScore("a")).toBe(1);
