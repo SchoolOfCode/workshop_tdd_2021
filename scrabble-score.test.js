ab308bf5b8fc16a8139620f4e31883ea0f236ed0
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2022-05-29 12:59:05 +0100

    Updating skeleton for test file

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,10 +1,4 @@
-import scrabbleScore from "./scrabble-score";
+import { calculateScrabbleScore } from "./scrabble-score";
 
 // List of matchers (expects) for jest:
 // https://jestjs.io/docs/using-matchers
-
-describe("scrabble score", () => {
-  it("should score 'a' correctly", () => {
-    expect(scrabbleScore("a")).toBe(1);
-  });
-});
